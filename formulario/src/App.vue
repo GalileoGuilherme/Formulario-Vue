3706a5c49ad42fd7943ee0ea55dd78f394be249c
Galileo Guilherme <galileoguilherme@outlook.com>
2022-12-20 23:39:04 -0300

    Update

---
+++ formulario/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="app">
+    <h1>Fazer Cadastro</h1>
+
+    <div>
+      <form>
+        <label>Nome: </label>
+        <input type="text" v-model="user.nome">
+        <br>
+
+        <label>Email: </label>
+        <input type="text" v-model="user.email">
+        <br>
+
+        <label>Qnt Func: </label>
+        <input type="text" v-model="user.qtdFunc">
+        <br>
+
+        <label>Opniao: </label>
+        <textarea v-model="opiniao"></textarea>
+        <br><br>
+
+        <label>Tecnologias</label><br>
+        <span><input type="checkbox" value="JavaScript" v-model="tecnologias"/>JavaScript</span><br>
+        
+        <span><input type="checkbox" value="Css" v-model="tecnologias"/>Css</span><br>
+
+        <span><input type="checkbox" value="VueJs" v-model="tecnologias"/>VueJs</span><br>
+
+      </form>
+    </div>
+    <hr>
+    <div class="item">
+      <h2>Usuario Cadastrado</h2>
+
+      <label>Nome: {{ user.nome }}</label><br>
+
+      <label>Email: {{ user.email }}</label><br>
+
+      <label>Qnt Func: {{ user.qtdFunc }}</label><br>
+
+      <label>Opiniao: {{ opiniao }}</label><br>
+
+      <label>Tecnologias: {{ tecnologias }}</label><br>
+
+      <ul>
+        <li v-for="tech in tecnologias" :key="tech">{{ tech }}</li>     
+      </ul>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data(){
+      return{
+        user:{
+          nome: '',
+          email: '',
+          qtdFunc: 0
+        },
+        opiniao: '',
+        tecnologias: []
+      }
+    }
+  }
+
+</script>
+
+<style>
+label{
+  display: flex;
+  flex-direction: column;
+}
+
+
+</style>
